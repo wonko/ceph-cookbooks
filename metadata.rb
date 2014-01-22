7a079512466eadbdd584e04446877fc4e7ffbb0b
Bernard Grymonpon <bernard@openminds.be>
2014-01-22 14:41:14 +0100

    removes dependency on apache2 cookbook
    We don't have that cookbook
    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name			"ceph"
 maintainer			"Kyle Bader"
 maintainer_email	"kyle.bader@dreamhost.com"
+maintainer_email	"bernard@openminds.be"
 license				"Apache 2.0"
-description			"Installs/Configures the Ceph distributed filesystem"
+description			"Installs/Configures the Ceph distributed filesystem - modified version by Bernard @ Openminds"
 long_description	IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version				"0.1.0"
-depends				"apache2", ">= 1.1.12"
+version				"0.1.1"
 depends 			"apt"
